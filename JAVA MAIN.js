544b49697233a4eefff0201d66ab26e10129fc3e
StepoverFutsal <muhammadzidan3bdg@gmail.com>
2024-10-20 16:12:23 +0700

    Add files via upload

---
+++ JAVA MAIN.js
@@ -0,0 +1,7 @@
+$("#Next").click(function(){
+	$("#Slider").append($("#u16 matchu16 matchu13 matchu10:first-of-type"));
+});
+
+$("#Prev").click(function(){
+	$("#Slider").prepend($("#u16 matchu16 matchu13 matchu10:last-of-type"));
+});
